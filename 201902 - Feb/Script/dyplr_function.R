cd86e026983c4dfbeb3fcec8956cb757e0511fd8
Giovanna De Vincenzo <YYI4544@ic.intra.infocamere.it>
2019-02-13 19:06:05 +0100

    script bozza
    script bozza

---
+++ 201902 - Feb/Script/dyplr_function.R
@@ -0,0 +1,156 @@
+#install.packages("readr")
+#install.packages("dplyr")
+#install.packages("tidyverse")
+
+
+#1. IMPORT LIBRARY
+library(readr)
+library(tidyverse)
+library(dplyr)
+
+Employee_Diversity_in_Tech <- read_delim("GitHub/RLadies/meetup-presentations_bari/2019 - Feb/dataset/Employee Diversity in Tech.csv", 
+                                         "\t", escape_double = FALSE, trim_ws = TRUE)
+
+# DATASET EXPLORATION
+summary(Employee_Diversity_in_Tech)
+glimpse(Employee_Diversity_in_Tech)
+
+head(Employee_Diversity_in_Tech)
+#head(Employee_Diversity_in_Tech, n=10)
+tail(Employee_Diversity_in_Tech)
+str(Employee_Diversity_in_Tech)
+
+names(Employee_Diversity_in_Tech)
+nrow(Employee_Diversity_in_Tech)
+ncol(Employee_Diversity_in_Tech)
+
+# random rows dal dataset
+sample_n(Employee_Diversity_in_Tech, 5)
+sample_frac(Employee_Diversity_in_Tech, 0.1)
+
+
+#Important Dplyr Functions
+
+# Function = Description
+# select() = Select columns
+# filter() = Filter rows
+# group_by() = Allows for group operations in the "split-apply-combine" concept
+# arrange() = Re-order or arrange rows
+# mutate() = Create new columns
+# summarize() = Summarize values
+
+##### SELECT 
+
+?select
+
+# Select Columns With Select()
+
+select(Employee_Diversity_in_Tech, data = Date, Tipo = Type, Azienda = Company)
+
+# To select all the columns except a specific column, use the "-" (subtraction) operator.
+
+head(select(Employee_Diversity_in_Tech,  -"% Undeclared"))
+
+# To select a range of columns by name, use the ":" (colon), operator
+head(select(Employee_Diversity_in_Tech, Sepal.Length:Petal.Length))
+
+# To select all columns that start with the character string "%", use the function starts_with()
+head(select(Employee_Diversity_in_Tech, starts_with("%")))
+
+# You can also select columns based on specific criteria with:
+   
+# ends_with() = Select columns that end with a character string
+# contains() = Select columns that contain a character string
+# matches() = Select columns that match a regular expression
+# one_of() = Select columns names that are from a group of names
+
+
+#remove %
+colnames(Employee_Diversity_in_Tech) <- gsub( "[\t\n\r\v\f %]", "", colnames(Employee_Diversity_in_Tech))
+colnames(Employee_Diversity_in_Tech)
+####### FILTER
+
+?filter()
+
+# Select Rows With Filter()
+
+# To filter the rows for Sepal.Length >= 4.6, you can use:
+  filter(Employee_Diversity_in_Tech, Sepal.Length >= 4.6)
+
+# To filter the rows for Sepal.Length >= 4.6 and Petal.Width >= 0.5, you can use:
+  filter(Employee_Diversity_in_Tech, Sepal.Length >= 4.6, Petal.Width>=0.5)
+
+###### Pipe Operator: %>%
+   
+#   Now we are going to talk about the pipe operator: %>%. 
+# This operator allows you to pipe the output from one function to the input of another function.
+# 
+# Let's pipe the Employee_Diversity_in_Tech data frame to the function that will select two columns 
+# Then it will pipe the new data frame to the function head(), which will return the head of the new data frame.
+
+  
+#####
+  Employee_Diversity_in_Tech %>%
+  select(Date, Type, Company) %>%
+  head()
+#####
+
+######### GROUP BY
+?group_by
+  
+  # Group Operations Using Group_by()
+  # 
+  # The group_by() verb is an important function in dplyr. We want to split the data frame by some variable (e.g. Sepal.Length), apply a function to the individual data frames, and then combine the output.
+  
+  # We split the iris data frame by the Sepal.Length, then ask for the same summary statistics as above.
+  iris %>%
+    group_by(Sepal.Length) %>%
+    summarise(avg_slength = mean(Sepal.Length),
+              min_slength = min(Sepal.Length),
+              max_slength = max(Sepal.Length),
+              total = n())
+  
+
+####### ARRANGE
+?arrange
+  
+# Arrange Rows With Arrange()
+# 
+# To arrange rows by a particular column, such as the 'Female %', 
+# list the name of the column you want to arrange the rows by.
+
+  Employee_Diversity_in_Tech %>% 
+    arrange(desc(`Female `)) %>% 
+    head()
+
+# Now, we will select three columns from iris, arrange the rows by Sepal.Length, then arrange the rows by Sepal.Width. Finally, we will show the head of the final data frame.
+iris %>%
+  select(Species, Sepal.Length, Sepal.Width) %>%
+  arrange(Sepal.Length, Sepal.Width) %>%
+  head
+
+
+
+########## MUTATE
+?mutate
+
+# Create New Columns With Mutate()
+# The mutate() function will add new columns to the data frame. Create a new column called proportion, which is the ratio of Sepal.Length to Sepal.Width.
+iris %>%
+  mutate(proportion = Sepal.Length / Sepal.Width) %>%
+  head
+
+
+######## SUMMARIZE
+?summarize
+
+# Create Summaries of the Data Frame With Summarize()
+# 
+# The summarize() function will create summary statistics for a given column in the data frame. For example, to compute the average number of Sepal.Length, apply the mean() function to the column Sepal.Length and call the summary value "avg_slength".
+iris %>%
+  summarize(avg_slength = mean(Sepal.Length))
+
+
+
+
+
