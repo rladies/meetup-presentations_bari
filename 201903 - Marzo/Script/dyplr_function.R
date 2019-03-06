f94800fad2dcd94548397a7337e36957c271f8ac
Giovanna De Vincenzo <YYI4544@ic.intra.infocamere.it>
2019-03-06 10:27:21 +0100

    update

--- 201903 - Marzo/Script/dyplr_function.R
+++ 201903 - Marzo/Script/dyplr_function.R
@@ -2,33 +2,32 @@
 #install.packages("dplyr")
 #install.packages("tidyverse")
 
-
 #1. IMPORT LIBRARY
 library(readr)
 library(tidyverse)
 #library(dplyr)
 
-Employee_Diversity_in_Tech <- read_delim("~/GitHub/RLadies/meetup-presentations_bari/201903 - Marzo/dataset/Employee Diversity in Tech.csv", 
+Employee_Diversity_in_Tech <- 
+  read_delim("~/GitHub/RLadies/meetup-presentations_bari/201903 - Marzo/dataset/Employee Diversity in Tech.csv", 
                                          ";", escape_double = FALSE, trim_ws = TRUE)
 
 
 #2. DATASET EXPLORATION
+summary(Employee_Diversity_in_Tech)
+glimpse(Employee_Diversity_in_Tech)
 
-summary()
-glimpse()
+head(Employee_Diversity_in_Tech)
+head(Employee_Diversity_in_Tech, n = 10)
+tail(Employee_Diversity_in_Tech)
+str(Employee_Diversity_in_Tech)
 
-head()
-#head(, n=10)
-tail()
-str()
-
-names()
-nrow()
-ncol()
+names(Employee_Diversity_in_Tech)
+nrow(Employee_Diversity_in_Tech)
+ncol(Employee_Diversity_in_Tech)
 
 # random rows dal dataset
-sample_n(, 5)
-sample_frac(, 0.1)
+sample_n(Employee_Diversity_in_Tech, 5)
+sample_frac(Employee_Diversity_in_Tech, 0.1)
 
 #3. DYPLR FUNCTIONS
 
@@ -47,22 +46,19 @@ sample_frac(, 0.1)
 
 # Select Columns With Select()
 
-select(Employee_Diversity_in_Tech, data = Date, Tipo = Type, Azienda = Company)
+selezione <- select(Employee_Diversity_in_Tech, Data = Date, Tipo = Type, Azienda = Company)
 
 # To select all the columns except a specific column, use the "-" (subtraction) operator.
 
 head(select(Employee_Diversity_in_Tech,  -"% Undeclared"))
 
 # To select a range of columns by name, use the ":" (colon), operator
-head(select(Employee_Diversity_in_Tech, Sepal.Length:Petal.Length))
+head(select(Employee_Diversity_in_Tech, Date:Company))
 
 # To select all columns that start with the character string "%", use the function starts_with()
 head(select(Employee_Diversity_in_Tech, starts_with("%")))
 
-
-
 # You can also select columns based on specific criteria with:
-   
 # ends_with() = Select columns that end with a character string
 # contains() = Select columns that contain a character string
 # matches() = Select columns that match a regular expression
@@ -72,6 +68,7 @@ head(select(Employee_Diversity_in_Tech, starts_with("%")))
 ###### EXTRA
 
 #remove % from colnames
+?gsub
 colnames(Employee_Diversity_in_Tech) <- gsub( "[\t\n\r\v\f %]", "", colnames(Employee_Diversity_in_Tech))
 colnames(Employee_Diversity_in_Tech)
 
@@ -80,11 +77,10 @@ colnames(Employee_Diversity_in_Tech)
 ?filter
 
 # Select Rows With Filter()
-
-# To filter the rows for Sepal.Length >= 4.6, you can use:
+# To filter the rows for you can use:
   filter(Employee_Diversity_in_Tech, Female > 50)
 
-# To filter the rows for Sepal.Length >= 4.6 and Petal.Width >= 0.5, you can use:
+# To filter the rows for , you can use:
   filter(Employee_Diversity_in_Tech, Female >= 50, Male >= 50)
 
 ###### Pipe Operator: %>%
@@ -99,7 +95,7 @@ colnames(Employee_Diversity_in_Tech)
 #####
   Employee_Diversity_in_Tech %>%
   select(Date, Type, Company) %>%
-  head()
+  head
 #####
 
   
@@ -113,20 +109,17 @@ colnames(Employee_Diversity_in_Tech)
 # We want to split the data frame by some variable (e.g. Type), 
 # apply a function to the individual data frames, and then combine the output.
 # We split the iris data frame by the Company, then ask for the same summary statistics as above.
-
-  
-  Employee_Diversity_in_Tech %>%
+Employee_Diversity_in_Tech %>%
     group_by(Type) %>%
     summarise(avg_Female = mean(Female),
               avg_Male = mean(Male),
               total = n())
-  
- 
-   Employee_Diversity_in_Tech %>%
+
+Employee_Diversity_in_Tech %>%
     group_by(Company) %>%
     summarise(avg_Female = mean(Female),
               avg_Male = mean(Male),
-              total = n())
+              total = n()) 
   
    Employee_Diversity_in_Tech %>%
      group_by(Type, Company) %>%
@@ -155,41 +148,30 @@ colnames(Employee_Diversity_in_Tech)
   select(Date, Type, Company, Female, Asian) %>%
   arrange(Female, Asian) %>%
   head()
-
-
+  
 #######  MUTATE
-
-  
 ?mutate
-
 # Create New Columns With Mutate()
-  
 # The mutate() function will add new columns to the data frame. 
 # Create a new column called proportion, which is the ratio of Female to Male.
   Employee_Diversity_in_Tech %>%
   mutate(tot = Female + Male) %>%
   head
-
-
 ######## SUMMARISE
 ?summarise
-
 # Create Summaries of the Data Frame With Summarize()
-# 
+
 # The summarise() function will create summary statistics for a given column in the data frame. 
-# For example, to compute the average number of Sepal.Length, apply the mean() function 
-# to the column Sepal.Length and call the summary value "avg_slength".
 
   
   Employee_Diversity_in_Tech %>%
-    group_by(Type, Company) %>%
+    group_by(Company) %>%
     summarise(avg_Female = mean(Female),
               avg_Male = mean(Male),
               total = n()) %>%
-    arrange(desc(avg_Female))
+    arrange(desc(avg_Female))  %>%
+  head()
 
-
-  
 ### EXTRA
 # utilizzare il campo 'Date' per altre operazioni 
   
